92b65a9c32e8d61627b17325fbd3f4bab89b5c66
Jake Arkinstall <65358059+jake-arkinstall@users.noreply.github.com>
2025-06-20 23:01:08 +0100

    Further update guppy test to take into account new list format of parsed array results

--- selene-sim/python/tests/test_guppy.py
+++ selene-sim/python/tests/test_guppy.py
@@ -106,8 +106,8 @@ def test_print_array():
     runner = build(guppy.compile(main), "flip_n4_arr")
     expected = {
         "cs": [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
-        "is": tuple(range(100)),
-        "fs": tuple(i * 0.0625 for i in range(100)),
+        "is": list(range(100)),
+        "fs": list(i * 0.0625 for i in range(100)),
     }
 
     # run the simulation on Quest and Stim
