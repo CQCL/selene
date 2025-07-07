fc9ca9e2da18d7b772be8d69e5eb864fb305668a
Jake Arkinstall <65358059+jake-arkinstall@users.noreply.github.com>
2025-07-07 10:37:30 +0100

    Add test for state file cleanup

--- selene-sim/python/tests/test_debug.py
+++ selene-sim/python/tests/test_debug.py
@@ -1,3 +1,5 @@
+import pytest
+
 from guppylang import guppy
 from guppylang.std.builtins import array
 from guppylang.std.debug import state_result
@@ -67,3 +69,51 @@ def test_array_subscript_state():
         state = Quest.extract_states_dict(shot.entries)["array_state"]
         assert state.get_density_matrix()[1][1] == 1
         assert state.get_state_vector_distribution()[0].probability == 1
+
+
+@pytest.mark.parametrize("cleanup_param", [True, False, None])
+def test_state_cleanup(cleanup_param):
+    @guppy
+    def main() -> None:
+        qs = array(qubit() for _ in range(2))
+        for i in range(2):
+            x(qs[i])
+        state_result("array_state", qs[0])
+        discard_array(qs)
+
+    runner = build(guppy.compile(main), "array_state")
+    plugin = Quest()
+    shots = QsysResult(
+        runner.run_shots(
+            simulator=plugin,
+            n_qubits=2,
+            n_shots=1,
+        )
+    )
+    run_directory = runner.runs
+    individual_runs = list(run_directory.iterdir())
+    assert len(individual_runs) == 1, "Expected only one run directory"
+    individual_run = individual_runs[0]
+    run_artifact_dir = individual_run / "artifacts"
+    assert run_artifact_dir.exists(), "Expected artifacts directory to exist"
+    state_files_pre_parse = list(run_artifact_dir.glob("*.state"))
+    assert len(state_files_pre_parse) == 1
+
+    for shot in shots.results:
+        state_dict = (
+            Quest.extract_states_dict(shot.entries)
+            if cleanup_param is None
+            else Quest.extract_states_dict(shot.entries, cleanup=cleanup_param)
+        )
+        state = state_dict["array_state"]
+        assert state.get_density_matrix()[1][1] == 1
+        assert state.get_state_vector_distribution()[0].probability == 1
+        state_files_post_parse = list(run_artifact_dir.glob("*.state"))
+        if cleanup_param in [True, None]:
+            assert len(state_files_post_parse) == 0, (
+                "Expected no state files after cleanup"
+            )
+        else:
+            assert state_files_post_parse == state_files_pre_parse, (
+                "Expected artifacts to remain unchanged if cleanup is False"
+            )
