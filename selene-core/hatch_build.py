a65ce795c4d6b0507bceb80518b25a23b2df093f
Jake Arkinstall <65358059+jake-arkinstall@users.noreply.github.com>
2025-07-08 10:08:55 +0100

    fix: correct inclusion of selene-core include files (#21)
    - Provides include files in selene-core for use by C-based extensions at build time.
    - As selene-core is now a little more intensive we build it separately and pass it into the CIBW context.

--- selene-core/hatch_build.py
+++ selene-core/hatch_build.py
@@ -22,7 +22,7 @@ class ExampleHeaders:
                 "--config",
                 "cbindgen.toml",
                 "--output",
-                f"{self.hook.root}/python/selene_core/_dist/include/selene_core/core_types.h",
+                f"{self.hook.root}/python/selene_core/_dist/include/selene/core_types.h",
             ],
             cwd=self.hook.root,
             stdout=subprocess.PIPE,
@@ -48,11 +48,18 @@ class ExampleHeaders:
             cwd=Path(self.hook.root) / "examples" / what,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
+            text=True,
         )
         stdout, stderr = p.communicate()
+        if p.returncode != 0:
+            self.hook.app.display_error(
+                f"cargo expand failed with return code {p.returncode}"
+            )
+            self.hook.app.display_error(stderr)
+            sys.exit(1)
         temp_rs_file = Path(tempfile.gettempdir()) / "expanded.rs"
         with temp_rs_file.open("w") as f:
-            f.write(stdout.decode("utf-8"))
+            f.write(stdout)
         p = subprocess.Popen(
             [
                 "cbindgen",
@@ -61,7 +68,7 @@ class ExampleHeaders:
                 "--lang",
                 "C",
                 "-o",
-                f"{self.hook.root}/python/selene_core/_dist/include/selene_core/{what}.h",
+                f"{self.hook.root}/python/selene_core/_dist/include/selene/{what}.h",
                 f"{temp_rs_file}",
             ],
             cwd=Path(self.hook.root) / "examples" / what,
@@ -85,7 +92,7 @@ class SeleneCoreBuildHook(BuildHookInterface):
         header_extractor.run()
 
         artifacts = []
-        dist_dir = Path(self.root) / "python/_dist"
+        dist_dir = Path("python/selene_core/_dist")
         for artifact in dist_dir.rglob("*"):
             if artifact.is_file():
                 artifacts.append(str(artifact.as_posix()))
